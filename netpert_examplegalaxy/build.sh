4d7f5357ec4ffd96a20eac5f2b5350f46502a4da
pranhav16 <31160824+pranhav16@users.noreply.github.com>
2024-03-16 00:10:56 -0400

    Create build.sh

---
+++ netpert_examplegalaxy/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ./databases
+
+curl -O https://www.informatics.jax.org/downloads/reports/MRK_List2.rpt
+
+curl -O -k https://www.grnpedia.org/trrust/data/trrust_rawdata.human.tsv
