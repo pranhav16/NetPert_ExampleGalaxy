6ead02e1e09241ece6dff5148e8064f17e6fafc4
pranhav16 <pranhav16@gmail.com>
2024-03-17 21:40:44 -0400

    Update __init__.py

--- netpert_examplegalaxy/__init__.py
+++ netpert_examplegalaxy/__init__.py
@@ -3,45 +3,49 @@ import os.path
 import csv
 import pandas as pd
 
-# Define the base URL for the MGI API
-base_dir = os.path.abspath(os.path.dirname(__file__))
+def main():
+    # Define the base URL for the MGI API
+    base_dir = os.path.abspath(os.path.dirname(__file__))
 
-DBDIR = os.path.join(base_dir,  'databases')
-mouse_genesInfo_filepath = os.path.join(DBDIR, 'MRK_List2.rpt')
-human_genesInfo_filepath = os.path.join(DBDIR, 'trrust_rawdata.human.tsv')
+    DBDIR = os.path.join(base_dir,  'databases')
+    mouse_genesInfo_filepath = os.path.join(DBDIR, 'MRK_List2.rpt')
+    human_genesInfo_filepath = os.path.join(DBDIR, 'trrust_rawdata.human.tsv')
 
-mouse_genesInfo = pd.read_csv(mouse_genesInfo_filepath, sep='\t')
-human_genesInfo = pd.read_csv(human_genesInfo_filepath, sep='\t', header= None, dtype = str)
+    mouse_genesInfo = pd.read_csv(mouse_genesInfo_filepath, sep='\t')
+    human_genesInfo = pd.read_csv(human_genesInfo_filepath, sep='\t', header= None, dtype = str)
 
-mouse_symbols = (mouse_genesInfo.loc[:,'Marker Symbol'])
-human_symbols = human_genesInfo.loc[:,1]
-#print(human_symbols)
+    mouse_symbols = (mouse_genesInfo.loc[:,'Marker Symbol'])
+    human_symbols = human_genesInfo.loc[:,1]
+    #print(human_symbols)
 
-if len(sys.argv) == 1:
-    print("No gene listed")
-else:
-    geneSymbol = sys.argv[1] 
-
-    if geneSymbol in mouse_symbols.values:
-        resultStr = "Valid Mouse Gene"
-    elif geneSymbol in human_symbols.values:
-        resultStr = "Valid Human Gene"
+    if len(sys.argv) == 1:
+        print("No gene listed")
     else:
-        resultStr = "Not a mouse or human gene"
+        geneSymbol = sys.argv[1] 
+
+        if geneSymbol in mouse_symbols.values:
+            resultStr = "Valid Mouse Gene"
+        elif geneSymbol in human_symbols.values:
+            resultStr = "Valid Human Gene"
+        else:
+            resultStr = "Not a mouse or human gene"
 
 
 
 
-    if len(sys.argv) > 2:
-        size = sys.argv[2] + " size network"
-    else:
-        size = "reduced size network"
+        if len(sys.argv) > 2:
+            size = sys.argv[2] + " size network"
+        else:
+            size = "reduced size network"
 
-    filename = "results.csv"
-    data = [resultStr,size]
-    with open(filename, 'w+', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
+        filename = "results.csv"
+        data = [resultStr,size]
+        with open(filename, 'w+', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
 
             # Write data to the CSV file row by row
-        for row in data:
-            csvwriter.writerow([row])
+            for row in data:
+                csvwriter.writerow([row])
+
+if __name__ == '__main__':
+    exit(main())
